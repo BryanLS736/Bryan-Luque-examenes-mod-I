04ea5d6f9651e71d3025efae181962a0cd4067d6
BryanLS736 <bryanlserna07@gmail.com>
2024-09-01 10:44:30 -0500

    resolucion practica 1

---
+++ Practica 1/Ejercicio 1.py
@@ -0,0 +1,30 @@
+"""Escribimos nuestras variables"""
+
+nombre = 'Bryan'
+salario = 200
+edad = '18'
+compañia = 'Compañia de usuario'
+
+"""Imprimimos sus tipos de variables"""
+
+print("El tipo de variable de 'nombre' es: {}".format(type(nombre)))
+print("El tipo de variable de 'salario' es: {}".format(type(salario)))
+print("El tipo de variable de 'edad' es: {}".format(type(edad)))
+print("El tipo de variable de 'compañia' es: {}".format(type(compañia)))
+
+"""Convertimos la variable edad que es string a int"""
+
+edad_int = int(edad)
+print(type(edad_int))
+print(edad_int)
+
+"""Realizamos nuestra condicional"""
+
+if edad_int > 30:
+    print('Ustes tiene un bono de 10% en el mes de diciembre')
+    bono_final = (salario**2)+0.10*salario
+    print(bono_final)
+else:
+    print('Ustes tiene un bono del 5% en el mes de diciembre')
+    bono_final = (salario ** 2) + 0.05 * salario
+    print(bono_final)
